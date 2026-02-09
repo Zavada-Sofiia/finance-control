9a5487d1245a7377d9203ce195feb65dc7af6052
Zavada Sofiia <zavada.sofia@gmail.com>
2026-02-09 11:23:02 +0200

    Update database.py

--- database.py
+++ database.py
@@ -8,30 +8,15 @@ engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
 # --- Моделі Бази Даних ---
 class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
-    email: Optional[EmailStr] = None
+    email: str = Field(index=True, unique=True)
     hashed_password: str
     disabled: bool = Field(default=False)
 
-class Transaction(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    amount: float  # Додатні — дохід, від'ємні — витрати
-    category: str
-    description: Optional[str] = None
-    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    user_id: int = Field(foreign_key="user.id")
-
 # --- Моделі для API (Pydantic) ---
 class UserRead(SQLModel):
-    id: int
     username: str
-    email: Optional[EmailStr]
-
-class TransactionCreate(SQLModel):
-    amount: float
-    category: str
-    description: Optional[str] = None
+    email: str
 
 # Ініціалізація БД
 def create_db_and_tables():
